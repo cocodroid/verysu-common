a0c976dcca7caaefcef9bb419cd2ce54bb42a9f9
Cocodroid <2531075716@qq.com>
2017-11-13 10:02:30 +0800

    增加获取本地/本机IP地址方法

--- src/main/java/com/verysu/basic/util/IpUtil.java
+++ src/main/java/com/verysu/basic/util/IpUtil.java
@@ -1,7 +1,9 @@
 package com.verysu.basic.util;
 
 import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -55,4 +57,50 @@ public class IpUtil {
 	
 		return ip;
 	}
+
+    /**
+     * 获取本地/本机IP地址
+     * @return
+     * @throws Exception
+     */
+    public static InetAddress getLocalHostLANAddress() throws Exception {
+        try {
+            InetAddress candidateAddress = null;
+            // 遍历所有的网络接口
+            for (Enumeration ifaces = NetworkInterface.getNetworkInterfaces(); ifaces.hasMoreElements(); ) {
+                NetworkInterface iface = (NetworkInterface) ifaces.nextElement();
+                // 在所有的接口下再遍历IP
+                for (Enumeration inetAddrs = iface.getInetAddresses(); inetAddrs.hasMoreElements(); ) {
+                    InetAddress inetAddr = (InetAddress) inetAddrs.nextElement();
+                    if (!inetAddr.isLoopbackAddress()) {// 排除loopback类型地址
+                        if (inetAddr.isSiteLocalAddress()) {
+                            // 如果是site-local地址，就是它了
+                            return inetAddr;
+                        } else if (candidateAddress == null) {
+                            // site-local类型的地址未被发现，先记录候选地址
+                            candidateAddress = inetAddr;
+                        }
+                    }
+                }
+            }
+            if (candidateAddress != null) {
+                return candidateAddress;
+            }
+            // 如果没有发现 non-loopback地址.只能用最次选的方案
+            InetAddress jdkSuppliedAddress = InetAddress.getLocalHost();
+            return jdkSuppliedAddress;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) throws Exception {
+//        System.out.println(getLocalHostLANAddress().getHostName());
+//        System.out.println(getLocalHostLANAddress().getHostAddress());
+//        System.out.println(getLocalHostLANAddress().getCanonicalHostName());
+//        System.out.println(getLocalHostLANAddress().getAddress());
+//        System.out.println(IpUtil.class);
+//        System.in.read();
+    }
 }
